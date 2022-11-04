e615e7055527130776e19aaf568c916cde4c11ba
taukirazam16 <taukir.azam16@gmail.com>
2022-11-04 12:33:23 +0600

    fixed Timestamp

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ length = 25.0  # in meters
 # logger_name = "2_01_R_092022070000AM"
 # report_file_name = "2_01_R_092022070000AM"
 
-video_name = "test_5min.avi"
+video_name = "2_01_R_092022070000AM.avi"
 logger_name = video_name.split(".")[0]
 report_file_name = logger_name
-
+starting_time = 7
 
 # Flags to annotate or save the annotated video
 show_video = False
@@ -36,7 +36,7 @@ tracker = t.VehicleTracker()
 camera = c.Camera("Test", os.path.join("Data", video_name), roi)
 frame_skipper = u.FrameSkipper(1)
 logger = u.Logger(os.path.join("Data"), logger_name)
-reporter = u.Reporter(os.path.join("Data", "Reports"), report_file_name, os.path.join("Data", "Frames"), logger, ['ID', 'timestamp', 'speed(km/h)'])
+reporter = u.Reporter(os.path.join("Data", "Reports"), report_file_name, os.path.join("Data", "Frames"), logger, ['ID', 'timestamp', 'speed(km/h)'], int(starting_time))
 speed = s.Speed(entry_area, exit_area, deleting_line, length, logger)
 
 if save_video:
